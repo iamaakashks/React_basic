88c1152a56412df294bf9db4114427e53687e5c9
IAmGit@06 <aakashkumarsumanaksgi7@gmail.com>
2024-08-13 22:01:27 +0530

    learned building components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Card from "./components/Card.jsx";
 function App(){
   return (
-    <Card />
+    <>
+      <Card/>
+      {/* <div>
+        {data.map((elem, index)=>(<div key={index} className='px-3 py-4 bg-zinc-300 rounded-md w-fit'>{elem}</div>))}
+      </div> */}
+      <h1>Hello</h1>
+    </>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 function Card(){
+
+    const data = [
+        {image: "https://images.unsplash.com/photo-1512469082103-409afe900e9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D" , name: "Meera Ratia", description:"Lorem ipsum dolor sit amet."},
+        {image: "https://plus.unsplash.com/premium_photo-1664882424626-f2f4cb557527?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D", name:"Ipsita Raina", description:"Lorem ipsum dolor sit amet consectetur."},
+        {image: "https://images.unsplash.com/photo-1617632679997-4382a0f0ff78?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D", name: "Rubina Sasse", description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."}
+    ]
+
     return (
-        <div className="w-full h-screen bg-zinc-400">
-            <div className='w-52 absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] rounded-md overflow-hidden'>
-                <div className='w-52 h-32 bg-zinc-200 '>
-                    <img className='w-full h-full object-cover' src="https://plus.unsplash.com/premium_photo-1708110921398-1fc68e98eacc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWxzfGVufDB8fDB8fHww" alt="" />
+        <div className="w-full h-screen flex py-[2.5%] justify-center gap-8 bg-zinc-400">
+            {data.map((elem, index)=>(
+                <div key={index} className='w-52 rounded-md overflow-hidden'>
+                    <div className='w-52 h-32 bg-zinc-200 '>
+                        <img className='w-full h-full object-cover' src={elem.image} alt="" />
+                    </div>
+                    <div className='rounded-md rounded-t-none text w-full h-1/6 px-3 py-4 font-["open_sans"] bg-yellow-200'>
+                        <h2 className='font-black'>{elem.name}</h2>
+                        <p className='text-xs mt-2'>{elem.description}</p>
+                    </div>
                 </div>
-                <div className='text w-full px-3 py-4 font-["open_sans"] bg-yellow-200'>
-                    <h2 className='font-black'>Ipsita Rhea</h2>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet.</p>
-                </div>
-                
-            </div>
+            ))}
+            
         </div>
+        
     )
 }
 export default Card;
